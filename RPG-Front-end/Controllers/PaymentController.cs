379a5207c031325984a8912f2083b0d9c1735ad1
Datdater <datnqse173081@fpt.edu.vn>
2025-03-18 23:44:02 +0700

    first commit

---
+++ RPG-Front-end/Controllers/PaymentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RPG_Front_end.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ExcutePayment(string orderCode)
+        {
+            return View("ExcutePayment", orderCode);
+        }
+    }
+}
---
+++ RPG-Front-end/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RPG_Front_end.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
